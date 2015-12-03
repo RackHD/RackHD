19e0aa87a8a3760b4d241f47a819dc0870887b41
Joe Heck <joseph.heck@emc.com>
2015-12-02 17:12:02 -0800

    partial fix to demo setup
    functional, but partial fix to demo setup. Updated instructions rely on
    https://github.com/RackHD/on-http/pull/56 and
    https://github.com/RackHD/on-http/pull/57, and the on-http submodule to
    be updated to include both of those commits.
    
     - resets the default SSH key for remote access (note: CoreOS SSH login not
       working per these instructions - not sure what I'm missing and haven't
       debugged login yet)
    
     - adding instructions and quick-links to download and unpack ESXi and
       CentOS7 iso images, and link them into the vagrant install
    
     - in support of the annotations in
       https://github.com/RackHD/docs/pull/203 to point readers to this
       local README for quick-setup instructions

---
+++ scripts/reset_submodules.bash
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# enable to see script debug output
+#set -x
+
+SCRIPT_DIR=$(cd $(dirname $0) && pwd)
+echo "Resetting all submodules to latest master branch"
+
+REPOS="on-core on-tasks on-dhcp-proxy on-http on-syslog on-tftp on-taskgraph"
+for repo in ${REPOS}; do
+    pushd "${SCRIPT_DIR}/../${repo}"
+    git fetch --all --prune
+    git reset --hard origin/master
+    popd
+done
