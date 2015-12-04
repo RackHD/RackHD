68bcf5a1a8138f486d3453a14e1847d6786a173c
Joe Heck <joseph.heck@emc.com>
2015-12-04 13:48:16 -0800

    enable local sudo without passwords

--- packer/scripts/dep.sh
+++ packer/scripts/dep.sh
@@ -6,4 +6,7 @@ apt-get -y update
 # to support using the ansible provisioner
 apt-get -y install ansible
 
+# enable sudo local access to anyone logging in (for ansible)
+echo '%sudo    ALL=(ALL)  NOPASSWD:ALL' >> /etc/sudoers
+
 # You can install anything you need here.
