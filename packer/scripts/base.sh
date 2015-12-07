b22bcbb5605499d6f0cee0740653ec9aad7039d1
Joe Heck <joseph.heck@emc.com>
2015-12-07 15:20:27 -0800

    moving curl to debseed portion of setup

--- packer/scripts/base.sh
+++ packer/scripts/base.sh
@@ -15,7 +15,7 @@ EOAPT
 
 # Update the box
 apt-get -y update >/dev/null
-apt-get -y install facter linux-headers-$(uname -r) build-essential zlib1g-dev libssl-dev libreadline-gplv2-dev curl unzip >/dev/null
+apt-get -y install facter linux-headers-$(uname -r) build-essential zlib1g-dev libssl-dev libreadline-gplv2-dev unzip >/dev/null
 
 # Tweak sshd to prevent DNS resolution (speed up logins)
 echo 'UseDNS no' >> /etc/ssh/sshd_config
