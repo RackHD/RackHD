7cfadcf38147a7f5695381bd78691f7e65b6c73f
Joe Heck <heckj@mac.com>
2015-12-08 14:58:06 -0800

    setting hostname

--- packer/scripts/dep.sh
+++ packer/scripts/dep.sh
@@ -6,4 +6,6 @@ apt-get -y update
 # to support using the ansible provisioner
 apt-get -y install ansible
 
-# You can install anything you need here.
+# set a friendly hostname
+rm -f /etc/hostname
+echo "rackhd-demo" > /etc/hostname
