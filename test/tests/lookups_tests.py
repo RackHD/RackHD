408ff3276cbd674c1ec93dd1d6f31359288717ea
tannoa2 <azzam.tannous@emc.com>
2016-01-12 14:14:59 +0000

    Added Lookups test

--- test/tests/lookups_tests.py
+++ test/tests/lookups_tests.py
@@ -20,6 +20,13 @@ class LookupsTests(object):
 
     def __init__(self):
         self.__client = config.api_client
+        self.lookup =   {
+                            "ipAddress": "11.11.11.11",
+                            "macAddress": "ae:aa:aa:aa:aa:aa",
+                            "node":"456"
+                        }
+        self.id = ""
+        self.patchedNode= {"node": "666" }
 
     @test(groups=['lookups.tests', 'check-lookups-query'], depends_on_groups=['nodes.tests'])
     def check_lookups_query(self):
@@ -39,7 +46,7 @@ class LookupsTests(object):
             assert_equal(200, rsp.status, message=rsp.reason)
             assert_not_equal(0, len(rsp.data))
 
-    @test(depends_on_groups=['check-lookups-query'])
+    @test(groups=['check-lookup-id'],depends_on_groups=['check-lookups-query'])
     def check_lookup_id(self):
         """ Testing GET:/lookups/:id """
         Nodes().api1_1_nodes_get()
@@ -61,5 +68,66 @@ class LookupsTests(object):
                 Lookups().api1_1_lookups_id_get(id.get('id'))
                 rsp = self.__client.last_response
                 assert_equal(200, rsp.status, message=rsp.reason)
+
+    @test(groups=['check-post-lookup'],depends_on_groups=['check-lookups-query'])
+    def post_lookup(self):
+        """ Testing POST """
+
+        #Validate that the lookup has not been posted from a previous test
+        Lookups().api1_1_lookups_get(self.lookup.get("macAddress"))
+        rsp = loads(self.__client.last_response.data)
+        listLen =len(rsp)
+        for i, val in enumerate (rsp):
+            if ( self.lookup.get("macAddress") ==  str (rsp[i].get('macAddress'))  ):
+                    LOG.info("Deleting the lookup with the same info before we post again")
+                    deleteID= str (rsp[i].get('id'))
+                    Lookups().api1_1_lookups_id_delete(deleteID)
+
+
+        #Add a lookup
+        Lookups().api1_1_lookups_post(self.lookup)
+        rsp = self.__client.last_response
+        assert_equal(200, rsp.status, message=rsp.reason)
+        self.id = str (loads(rsp.data).get('id'))
+        LOG.info("ID is "+ self.id)
+
+        #Validate the content
+        Lookups().api1_1_lookups_get(self.lookup.get("macAddress"))
+        rsp = loads(self.__client.last_response.data)
+        assert_equal(str(rsp[0].get("ipAddress")),str(self.lookup.get("ipAddress")))
+        assert_equal(str(rsp[0].get("macAddress")),str(self.lookup.get("macAddress")))
+        assert_equal(str(rsp[0].get("node")),str(self.lookup.get("node")))
+
+    @test(groups=['check-post-lookup-negativeTesting'],depends_on_groups=['check-lookups-query','check-post-lookup'])
+    def post_lookup_negativeTesting(self):
+        """ Negative Testing POST """
+        #Validate that a POST for a lookup with same id as an existing one gets rejected
+        try:
+            Lookups().api1_1_lookups_post(self.lookup)
+        except Exception,e:
+           assert_equal(500,e.status, message = 'status should be 505')
+
+    @test(groups=['check-patch-lookup'],depends_on_groups=['check-lookups-query','check-post-lookup','check-post-lookup-negativeTesting'])
+    def patch_lookup(self):
+        """ Testing PATCH """
+        Lookups().api1_1_lookups_id_patch(self.id,self.patchedNode)
+
+        #validate that the node element has been updated
+        Lookups().api1_1_lookups_get(self.lookup.get("macAddress"))
+        rsp = loads(self.__client.last_response.data)
+        assert_equal(str(rsp[0].get("node")),self.patchedNode.get("node") )
+
+    @test(groups=['check-delete-lookup'], depends_on_groups=['check-lookups-query','check-post-lookup','check-patch-lookup'])
+    def delete_lookup(self):
+        """ Testing DELETE """
+        LOG.info("The lookup ID to be deleted is "+ self.id)
+        Lookups().api1_1_lookups_id_delete(self.id)
+        rsp = self.__client.last_response
+        assert_equal(200, rsp.status, message=rsp.reason)
+
+        #Validate that the lookup has been deleted and the returned value is an empty list
+        Lookups().api1_1_lookups_get(self.lookup.get("macAddress"))
+        rsp = loads(self.__client.last_response.data)
+        assert_equal(len (rsp),0)
 
 
