b081a9cb78ac7ff716c59116af3debb2f4085808
Kranti Uppala <uppalk1@kranti.veyder.emc.com>
2016-01-12 14:01:20 -0500

    Update run.py script to include tests.config_tests

--- test/run.py
+++ test/run.py
@@ -1,14 +1,16 @@
 from proboscis import register
 from proboscis import TestProgram
 
+
 def run_tests():
     from tests.nodes_tests import NodesTests
     from tests.obm_tests import OBMTests
     from tests.amqp_tests import AMQPTests
     from tests.lookups_tests import LookupsTests
     from tests.profiles_tests import ProfilesTests
+    from tests.config_tests import ConfigTests
 
-    groups_list  = [
+    groups_list = [
             'integraton'
     ]
     depends_list = [
@@ -17,6 +19,7 @@ def run_tests():
             'amqp.tests',
             'lookups.tests',
             'profiles.tests'
+            'config.tests'
     ]
     register(groups=groups_list, depends_on_groups=depends_list)
     TestProgram().run_and_exit()
