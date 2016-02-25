8eb18bea6423ffca227e6e40f605e22f8c5324b8
Poulter, Roland <Roland.Poulter@emc.com>
2016-02-24 17:11:28 -0800

    Fix dhcp mounts

--- docker/dhcp/docker-entrypoint.sh
+++ docker/dhcp/docker-entrypoint.sh
@@ -4,11 +4,9 @@
 
 # set -x
 
-chown dhcp /var/lib/dhcp
-# chmod 777 /var/lib/dhcp
-
+rm -f /var/lib/dhcp/dhchp.leases
 touch /var/lib/dhcp/dhcpd.leases
-chown dhcp /var/lib/dhcp/dhcpd.leases
-# chmod 666 /var/lib/dhcp/dhcpd.leases
+chown root:root /var/lib/dhcp/dhcpd.leases
+chmod 666 /var/lib/dhcp/dhcpd.leases
 
 dhcpd -f -cf /etc/dhcp/dhcpd.conf -lf /var/lib/dhcp/dhcpd.leases --no-pid
