ac51a9a19e71362683b954bdb8ae3599c3a243e9
Winson Wang <winson.wang@emc.com>
2016-02-26 10:15:30 -0500

    Update auth module/test according the python client code update

--- test/modules/auth.py
+++ test/modules/auth.py
@@ -1,4 +1,4 @@
-from config.api1_1_config import *
+from config.api1_1_config import config
 from logger import Log
 from json import loads
 
@@ -8,7 +8,8 @@ class Auth(object):
     """
     Class to abstract python authentication functionality
     """
-    def get_auth_token(self):
+    @staticmethod
+    def get_auth_token():
         """ call /login to get auth_token """
         resource_path = '/login'
         method = 'POST'
@@ -16,30 +17,30 @@ class Auth(object):
             'username': 'admin',
             'password': 'admin123'
         }
+        config.api_client.host = config.host_authed
         config.api_client.call_api(resource_path, method, body=body_params)
         token_blob = loads(config.api_client.last_response.data)
         LOG.debug(token_blob, json=True)
         return token_blob['token']
 
-    def enable(self):
+    @staticmethod
+    def enable():
         """ update config to enable auth """
         if config.auth_enabled:
             LOG.info('auth already enabled.')
-        config.api_client_no_auth = config.api_client
-        config.api_client = config.api_client_authed
-        config.api_key = {'authorization' : self.get_auth_token()}
+        config.api_key = {'authorization' : Auth.get_auth_token()}
         config.api_key_prefix = {'authorization' : 'JWT'}
+        config.api_client.host = config.host_authed + config.api_root
         config.auth_enabled = True
         LOG.info('Enable auth successfully.')
 
-    def disable(self):
+    @staticmethod
+    def disable():
         """ update config to disable auth """
         if not config.auth_enabled:
             LOG.info('auth already disabled.')
-        config.api_client_authed = config.api_client
-        config.api_client = config.api_client_no_auth
-        del config.api_client_no_auth
         config.api_key = {}
         config.api_key_prefix = {}
+        config.api_client.host = config.host + config.api_root
         config.auth_enabled = False
         LOG.info('Disable auth successfully.')
--- test/tests/api/v1_1/auth_tests.py
+++ test/tests/api/v1_1/auth_tests.py
@@ -1,7 +1,7 @@
-from config.api1_1_config import *
+from config.api1_1_config import config
 from modules.logger import Log
 from modules.auth import Auth
-from on_http import NodesApi as Nodes
+from on_http_api1_1 import NodesApi as Nodes
 from proboscis.asserts import assert_equal
 from proboscis import before_class
 from proboscis import after_class
@@ -13,22 +13,22 @@ LOG = Log(__name__)
 class AuthTests(object):
 
     def __init__(self):
-        self.__auth = Auth()
+        self.__client = config.api_client
 
     @before_class()
     def setup(self):
         """setup test environment"""
-        self.__auth.enable()
+        Auth.enable()
 
     @after_class(always_run=True)
     def teardown(self):
         """ restore test environment """
-        self.__auth.disable()
+        Auth.disable()
 
     @test(groups=['test-nodes-withauth'])
     def test_nodes(self):
         """ Testing GET:/nodes """
-        Nodes().api1_1_nodes_get()
-        res = config.api_client.last_response
+        Nodes().nodes_get()
+        res = self.__client.last_response
         LOG.debug(res.data, json=True)
         assert_equal(200, res.status, message=res.reason)
