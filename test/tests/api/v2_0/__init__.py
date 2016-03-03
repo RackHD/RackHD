938171a356c8163f96d5346cda3815e6e115501c
Dale Bremner <dbremner@kickseed.veyder.emc.com>
2016-03-03 16:38:24 -0500

    Functional tests for 2.0 API OBM

--- test/tests/api/v2_0/__init__.py
+++ test/tests/api/v2_0/__init__.py
@@ -2,9 +2,11 @@
 from config_tests import ConfigTests
 from nodes_tests import NodesTests
 from catalogs_tests import CatalogsTests
+from obm_tests import OBMTests
 
 tests = [
     'nodes_api2.tests',
     'config_api2.tests',
     'catalogs_api2.tests'
+    'obm_api2.tests'
 ]
