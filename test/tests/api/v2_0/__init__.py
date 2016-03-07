6b63d78d860d6aff0e23cea0dd69df748ef714f0
onrack <onrack@ORA.hwimo.lab.emc.com>
2016-03-07 15:28:35 +0000

    Catalogs functional test

--- test/tests/api/v2_0/__init__.py
+++ test/tests/api/v2_0/__init__.py
@@ -1,9 +1,10 @@
 # import tests
 from config_tests import ConfigTests
 from nodes_tests import NodesTests
-
+from catalogs_tests import CatalogsTests
 
 tests = [
     'nodes_api2.tests',
-    'config_api2.tests'
+    'config_api2.tests',
+    'catalogs_api2.tests'
 ]
