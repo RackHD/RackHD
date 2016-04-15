aea247a7a335a3cf2fb0804e453cccb91791b2ac
Kranti Uppala <uppalk1@kickseed.veyder.emc.com>
2016-04-15 12:13:07 -0400

    Addressed PR comments

--- test/tests/api/v2_0/tags_tests.py
+++ test/tests/api/v2_0/tags_tests.py
@@ -9,6 +9,7 @@ from proboscis import test
 from proboscis.asserts import assert_raises
 from nodes_tests import NodesTests
 from json import loads
+from json import dumps
 from time import sleep
 
 LOG = Log(__name__)
@@ -24,69 +25,85 @@ class TagsTests(object):
     @test(groups=['create-tag-api2'])
     def test_tag_create(self):
         """ Testing POST:/api/2.0/tags/ """
-        tagsWithRules = {"name": "new-tag",
-                         "rules": [{"equals": "QuantaPlex T41S-2U",
-                                    "path": "dmi.System Information.Product Name"}]}
-        LOG.info('Creating tag (name={0})'.format(tagsWithRules))
-        Api().create_tag(body=tagsWithRules)
-        rsp = self.__client.last_response
-        LOG.info("Printing response")
-        LOG.info(rsp.status, json=False)
-        LOG.info(rsp.data)
-        assert_equal(201, rsp.status, message=rsp.reason)
+        Api().nodes_get_all()
+        nodes = self.__get_data()
+        for n in nodes:
+            if n.get('type') == 'compute':
+                Api().nodes_get_catalog_by_id(identifier=n.get('id'))
+                updated_catalog = self.__get_data()
+                tagsWithRules = {"name": n.get('id'),
+                                 "rules":[{"equals": updated_catalog[0]["data"]["System Information"]["Manufacturer"],
+                                           "path": "dmi.System Information.Manufacturer" }]}
+                LOG.info(tagsWithRules)
+                Api().create_tag(body=tagsWithRules)
+                tag_data = self.__get_data()
+                assert_equal(n.get('id'), tag_data["name"], "Failed creating tag")
+        assert_raises(rest.ApiException, Api().create_tag, body='fooey')
 
     @test(groups=['test-tags-api2'], depends_on_groups=['create-tag-api2'])
     def test_tags(self):
         """ Testing GET:/api/2.0/tags """
+        Api().nodes_get_all()
+        nodes = self.__get_data()
+        tagsArray = []
+        for n in nodes:
+            if n.get('type') == 'compute':
+                Api().nodes_get_catalog_by_id(identifier=n.get('id'))
+                updated_catalog = self.__get_data()
+                tagsWithRules = {"name": n.get('id'),
+                                 "rules":[{"equals": updated_catalog[0]["data"]["System Information"]["Manufacturer"],
+                                           "path": "dmi.System Information.Manufacturer" }]}
+                tagsArray.append(tagsWithRules)
         Api().get_all_tags()
         rsp = self.__client.last_response
-        LOG.info("Printing response")
-        LOG.info(rsp.status, json=False)
-        updated_tags = loads(rsp.data)
+        updated_tags = self.__get_data()
         assert_equal(200, rsp.status, message=rsp.reason)
-        assert_equal(updated_tags[0]['name'], 'new-tag', message='Could not find the tag')
+        for i in xrange(len(updated_tags)):
+            assert_equal(updated_tags[i]['rules'][0]['path'], 'dmi.System Information.Manufacturer', message='Could not find the tag')
 
     @test(groups=['test-tags-tagname-api2'], depends_on_groups=['test-tags-api2'])
     def test_tags_tagname(self):
         """ Testing GET:/api/2.0/tags/:tagName """
-        Api().get_tag(tag_name="new-tag")
-        tag = self.__get_data()
-        LOG.info(tag, json=True)
-        rsp = self.__client.last_response
-        LOG.info("Printing response")
-        LOG.info(tag.get('name'))
-        LOG.info(rsp.status, json=False)
-        assert_equal(200, rsp.status, message=rsp.reason)
-        assert_equal(tag.get('name'), 'new-tag', message='Could not find the tag')
-        assert_equal(tag.get('rules'),
-                     [{"equals": "QuantaPlex T41S-2U","path": "dmi.System Information.Product Name"}],
-                     message='Could not find the tag')
+        Api().nodes_get_all()
+        nodes = self.__get_data()
+        for n in nodes:
+            if n.get('type') == 'compute':
+                Api().get_tag(tag_name=n.get("id"))
+                rsp = self.__client.last_response
+                tag = self.__get_data()
+                assert_equal(200, rsp.status, message=rsp.reason)
+                assert_equal(tag.get('name'), n.get('id'), message='Could not find the tag')
 
     @test(groups=['test-nodes-tagname-api2'], depends_on_groups=['test-tags-tagname-api2'])
     def test_nodes_tagname(self):
         """ Testing GET:/api/2.0/tags/:tagName/nodes """
-        Api().get_nodes_by_tag(tag_name='new-tag')
+        Api().nodes_get_all()
         nodes = self.__get_data()
         for n in nodes:
-            LOG.info(n.get('id'), json=True)
-        rsp = self.__client.last_response
-        nodesList = loads(rsp.data)
-        tagsList = nodesList[0]['tags']
-        checkTag = 'new-tag' in tagsList
-        assert_equal(True, checkTag, message=rsp.reason)
+            Api().get_nodes_by_tag(tag_name=n.get('id'))
+            nodesWithTags = self.__get_data()
+            for n in nodesWithTags:
+                rsp = self.__client.last_response
+                nodesList = self.__get_data()
+                tagsList = nodesList[0]['tags']
+                checkTag = n.get('id') in tagsList
+                assert_equal(True, checkTag, message=rsp.reason)
 
     @test(groups=['test_tags_delete'], depends_on_groups=['test-nodes-tagname-api2'])
     def test_tags_del(self):
         """ Testing DELETE:api/2.0/tags/:tagName """
-        LOG.info("Deleting 'new-tag' that was created in"
+        LOG.info("Deleting tag that was created in"
                  " Testing POST:/api/2.0/tags/ ")
-        Api().delete_tag(tag_name='new-tag')
-        rsp = self.__client.last_response
-        assert_equal(204, rsp.status, message=rsp.reason)
-        Api().get_all_tags()
-        rsp = self.__client.last_response
-        updated_tags = loads(rsp.data)
-        assert_equal(200, rsp.status, message=rsp.reason)
-        assert_equal([],updated_tags, message='new-tag was not deleted successfully')
+        Api().nodes_get_all()
+        nodes = self.__get_data()
+        for n in nodes:
+            Api().delete_tag(tag_name=n.get('id'))
+            rsp = self.__client.last_response
+            assert_equal(204, rsp.status, message=rsp.reason)
+            Api().get_all_tags()
+            rsp = self.__client.last_response
+            updated_tags = self.__get_data()
+            assert_equal(200, rsp.status, message=rsp.reason)
+        assert_equal([],updated_tags, message='Tags were not deleted successfully')
 
 
