b61e66b75e64c5826a53c3b932d173e9f1aab8c0
Lyne Lin <lyne.lin@emc.com>
2016-04-28 09:15:12 -0400

    Add benchmark node discovery case

--- test/benchmark.py
+++ test/benchmark.py
@@ -8,11 +8,10 @@ def run_tests(api_ver, selected):
     if api_ver == '2':
         import benchmark.api_v2_0 as benchmark
     else:
-        import benchmark.api_v1_1 as benchmark
+        import benchmark.api_v1_1_tests
 
-    register(groups=['poller'], depends_on_groups=benchmark.benchmark_poller_tests)
-    register(groups=['discovery'], depends_on_groups=benchmark.benchmark_discovery_tests)
-    register(groups=['bootstrap'], depends_on_groups=benchmark.benchmark_bootstrap_tests)
+    register(groups=['poller'], depends_on_groups=['benchmark.poller'])
+    register(groups=['discovery'], depends_on_groups=['benchmark.discovery'])
 
     if selected == False:
         TestProgram(groups=['poller','discovery','bootstrap']).run_and_exit()
@@ -26,19 +25,12 @@ if __name__ == '__main__':
     api_version = "1"
     group_selected = False
 
-    try:
-        opts, args = getopt.getopt(sys.argv[1:], "h", ["api_version=", "group="])
-        for op, value in opts:
-            if op == "--api_version":
-                # Remove this arg from array to prevent TestProgram processing it
-                sys.argv = filter(lambda x: x != op+'='+value, sys.argv)
-                api_version = value
-            if op == '--group':
-                group_selected = True
-            if op == "-h":
-                print "Usage: benchmark.py [--api_version|--group]"
-                exit()
-    except getopt.GetoptError:
-        sys.exit("option or arg is not supported")
+    for arg in sys.argv[1:]:
+        if arg[:14] == '--api_version=':
+            # Remove this arg from array to prevent TestProgram processing it
+            sys.argv = filter(lambda x: x != op+'='+value, sys.argv)
+            api_version = value
+        elif arg[:8] == '--group=':
+            group_selected = True
 
     run_tests(api_version, group_selected)
--- test/tests/api/v1_1/nodes_tests.py
+++ test/tests/api/v1_1/nodes_tests.py
@@ -352,7 +352,7 @@ class NodesTests(object):
                     data = self.__get_data()
                 resps.append({'data': data, 'id':id})
         for resp in resps:
-            assert_not_equal(0, len(resp['data']), 
+            assert_not_equal(0, len(resp['data']),
                 message='No Workflows found for Node {0}'.format(resp['id']))
         assert_raises(rest.ApiException, Nodes().nodes_identifier_workflows_post, 'fooey','Graph.Discovery',body={})
 
@@ -385,4 +385,38 @@ class NodesTests(object):
                         timeout -= 1
         assert_raises(rest.ApiException, Nodes().nodes_identifier_workflows_active_delete, 'fooey')
 
+    @test(groups=['node_workflows_finish'], depends_on_groups=["test_node_workflows_post_reboot"])
+    def test_node_workflows_finish(self):
+        """Wait for :id/workflows/active to be updated to empty"""
+        Nodes().nodes_get()
+        nodes = loads(self.__client.last_response.data)
 
+        for n in nodes:
+            if n.get('type') == 'compute':
+                id = n.get('id')
+                assert_not_equal(id,None)
+                status = self.__get_workflow_status(id)
+                timeout = 20
+                while status == 'pending' and timeout != 0:
+                    LOG.warning('Workflow status for Node {0} (status={1},timeout={2})'.format(id,status,timeout))
+                    status = self.__get_workflow_status(id)
+                    sleep(5)
+                    timeout -= 1
+                assert_not_equal(0, timeout, message="Workflow didn't end!")
+
+    @test(groups=['delete_all_node'],
+            depends_on_groups=["node_workflows_finish", "test-bm-discovery-prepare"])
+    def test_node_delete_all(self):
+        """ Testing DELETE all compute nodes """
+        codes = []
+        Nodes().nodes_get()
+        nodes = self.__get_data()
+        for n in nodes:
+            if n.get('type') == 'compute':
+                uuid = n.get('id')
+                Nodes().nodes_identifier_delete(uuid)
+                codes.append(self.__client.last_response)
+
+        assert_not_equal(0, len(codes), message='Delete node list empty!')
+        for c in codes:
+            assert_equal(200, c.status, message=c.reason)
