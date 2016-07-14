d6a0a4010a18936fb3fd6cd6894bb6b4b151060e
Joe Heck <heckj@mac.com>
2016-07-14 09:03:20 -0700

    added missing on-imagebuilder repo
    missing from submodule/reset update script

--- scripts/reset_submodules.bash
+++ scripts/reset_submodules.bash
@@ -6,7 +6,7 @@
 SCRIPT_DIR=$(cd $(dirname $0) && pwd)
 echo "Resetting all submodules to latest master branch"
 
-REPOS="on-core on-tasks on-dhcp-proxy on-http on-statsd on-syslog on-tftp on-taskgraph on-wss"
+REPOS="on-core on-tasks on-dhcp-proxy on-http on-statsd on-syslog on-tftp on-taskgraph on-wss on-imagebuilder"
 for repo in ${REPOS}; do
     pushd "${SCRIPT_DIR}/../${repo}"
     git fetch --all --prune
