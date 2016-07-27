9fd4500922fd3a7597a0f5544f6beb17a0f85e9d
Joe Heck <heckj@mac.com>
2016-07-27 11:31:16 -0700

    adding on-tools and imagebuilder to example update scripts

--- scripts/clean_all.bash
+++ scripts/clean_all.bash
@@ -6,7 +6,7 @@
 SCRIPT_DIR=$(cd $(dirname $0) && pwd)
 echo "Cleaning installation of all submodules"
 
-REPOS="on-core on-tasks on-dhcp-proxy on-http on-statsd on-syslog on-tftp on-taskgraph on-wss"
+REPOS="on-core on-tasks on-dhcp-proxy on-http on-statsd on-syslog on-tftp on-taskgraph on-wss on-imagebuilder on-tools"
 for repo in ${REPOS}; do
     if [ -d "${SCRIPT_DIR}/../${repo}/node_modules" ]; then
         rm -rf "${SCRIPT_DIR}/../${repo}/node_modules"
