8153d75b93051f8b5c6220f8e9739ae5a613f45c
John H <john.hogenmiller@emc.com>
2016-07-28 14:59:04 -0400

    Group vars for vagrant environment.

--- test/ansible/rackhd-ansible.bash
+++ test/ansible/rackhd-ansible.bash
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-sudo ansible-playbook -i /vagrant/ansible/inventory-vagrant.ini /opt/ansible/rackhd/rackhd_package.yml
+sudo ansible-playbook -i /vagrant/ansible/inventory-vagrant.ini /opt/ansible/rackhd/rackhd_package.yml $@
