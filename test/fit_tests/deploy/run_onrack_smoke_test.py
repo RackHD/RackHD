2dfe4e217b600148e725819c12bbb09405022859
paulog <george.paulos@emc.com>
2016-08-22 20:55:45 +0000

    Update deploy/run_onrack_smoke_test.py to deploy OnRack OVA

--- test/fit_tests/deploy/run_onrack_smoke_test.py
+++ test/fit_tests/deploy/run_onrack_smoke_test.py
@@ -14,17 +14,19 @@ import subprocess
 sys.path.append(subprocess.check_output("git rev-parse --show-toplevel", shell=True).rstrip("\n") + "/test/fit_tests/common")
 import fit_common
 
+# Check for -template parameter for OnRack OVA
+if fit_common.ARGS_LIST["template"] == fit_common.GLOBAL_CONFIG['repos']['install']['template']:
+    print "ERROR: '-template' parameter required for path or URL to OnRack OVA."
+    sys.exit(255)
+
 class onrack_smoke_test(fit_common.unittest.TestCase):
     def test01_install_os_ova(self):
         self.assertEqual(fit_common.run_nose(fit_common.TEST_PATH + '/deploy/os_ova_install.py'), 0, 'OS installer failed.')
 
-    def test02_rackhd_installer(self):
-        self.assertEqual(fit_common.run_nose(fit_common.TEST_PATH + '/deploy/onrack_package_install.py'), 0, 'OnRack installer failed.')
-
-    def test03_initialize_stack(self):
+    def test02_initialize_stack(self):
         self.assertEqual(fit_common.run_nose(fit_common.TEST_PATH + '/deploy/onrack_stack_init.py'), 0, 'OnRack stack init failed.')
 
-    def test04_onrack_smoke_test(self):
+    def test03_onrack_smoke_test(self):
         # set test group to 'smoke'
         fit_common.ARGS_LIST['group'] = "smoke"
         self.assertEqual(fit_common.run_nose(fit_common.TEST_PATH + '/tests'), 0, 'OnRack Smoke Test failed.')
