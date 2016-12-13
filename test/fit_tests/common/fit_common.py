ea33c50e5090507fed2f95d66f2cc3a41ff6d840
Patel, Beena <Beenaben.Patel@emc.com>
2016-12-13 15:42:14 +0000

    Update os_ova_install for Multiple ova install

--- test/fit_tests/common/fit_common.py
+++ test/fit_tests/common/fit_common.py
@@ -38,6 +38,7 @@ ARGS_LIST = \
     "version": os.getenv("VERSION", "onrack-devel"), # code version
     "template": os.getenv("TEMPLATE", "None"), # path or URL link to OVA for deployment
     "xunit": os.getenv("XUNIT", False), # XUNIT output
+    "numvms" : os.getenv("NUMVMS", 1), # number of OVA for reployment
     "list": os.getenv("LIST", False), # list tests
     "group": os.getenv("GROUP", "all"), # test group
     "http": os.getenv("HTTP", "False"), # force http api protocol
@@ -872,6 +873,7 @@ def run_nose(nosepath):
                              'export VERSION=' + str(ARGS_LIST['version']) + ';' +
                              'export TEMPLATE=' + str(ARGS_LIST['template']) + ';' +
                              'export XUNIT=' + str(ARGS_LIST['xunit']) + ';' +
+                             'export NUMVMS=' + str(ARGS_LIST['numvms']) + ';' +
                              'export GROUP=' + str(ARGS_LIST['group']) + ';' +
                              'export CONFIG=' + str(ARGS_LIST['config']) + ';' +
                              'export HTTP=' + str(ARGS_LIST['http']) + ';' +
