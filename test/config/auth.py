86a21d6d61df5593251ed1772c7c5bf5cef8ef5b
Turnquist, James <James.Turnquist@emc.com>
2016-12-20 17:26:45 +0000

    Encapsulate config.ini inside global_config.json.
    This is a change that moves towards combining cit and fit
    configurations.  This will not be the final solution but allows
    both cit and fit to read from a common file.
    
    run.py will still read from config.ini and operate as it
    had been.
    
    The table is set for run_tests.py to run CIT tests by
    setting the FIT_CONFIG environment variable.
    
    Fix an issue where USER_AUTH_PORTS needed to be strings
    in order to be formatted in urls.

--- test/config/auth.py
+++ test/config/auth.py
@@ -2,5 +2,5 @@ from settings import defaults
 
 SSH_USER = defaults['RACKHD_SSH_USER']
 SSH_PASSWORD = defaults['RACKHD_SSH_PASSWORD']
-SSH_PORT = defaults['RACKHD_SSH_PORT']
-USER_AUTH_PORT = defaults['RACKHD_USER_AUTH_PORT']
+SSH_PORT = str(defaults['RACKHD_SSH_PORT'])
+USER_AUTH_PORT = str(defaults['RACKHD_USER_AUTH_PORT'])
