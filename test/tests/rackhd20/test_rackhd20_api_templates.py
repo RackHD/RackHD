b93efc6b2f37ab2fbc6cac4601b245e0764034d6
paulog <george.paulos@emc.com>
2016-12-30 15:30:45 +0000

    Updated to templates tests to address Jeanee's comments

--- test/tests/rackhd20/test_rackhd20_api_templates.py
+++ test/tests/rackhd20/test_rackhd20_api_templates.py
@@ -17,6 +17,7 @@ from nose.plugins.attrib import attr
 @attr(all=True, regression=True, smoke=True)
 class rackhd20_api_templates(fit_common.unittest.TestCase):
     def setUp(self):
+        # this clears out any existing instance of 'testid' template
         fit_common.rackhdapi("/api/2.0/templates/library/testid", action="delete")
 
     def test_api_20_templates_metadata(self):
@@ -42,12 +43,14 @@ class rackhd20_api_templates(fit_common.unittest.TestCase):
                 self.assertGreater(len(item[subitem]), 0, subitem + ' field error')
 
     def test_api_20_templates_library_ID_put_get_delete(self):
+        # this test creates a dummy template called 'testid', checks it, then deletes it
         api_data = fit_common.rackhdapi("/api/2.0/templates/library/testid?scope=global", action="text-put", payload="null")
         self.assertEqual(api_data['status'], 201, "Was expecting code 201. Got " + str(api_data['status']))
         api_data = fit_common.rackhdapi("/api/2.0/templates/library/testid")
         self.assertEqual(api_data['text'], "null", "Data 'null' was not returned.")
         self.assertEqual(api_data['status'], 200, "Was expecting code 200. Got " + str(api_data['status']))
         api_data = fit_common.rackhdapi("/api/2.0/templates/library/testid", action="delete")
+        self.assertEqual(api_data['status'], 200, "Was expecting code 200. Got " + str(api_data['status']))
 
 if __name__ == '__main__':
     fit_common.unittest.main()
