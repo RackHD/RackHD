1df06a3a60994d9cc3f2f9256040a6d4c056d19e
paulog <george.paulos@emc.com>
2017-02-10 21:30:06 +0000

    Commit RAC-3711-ora-reference-to-rackhd_host

--- test/common/fit_common.py
+++ test/common/fit_common.py
@@ -1,11 +1,11 @@
 '''
-Copyright 2016, EMC, Inc.
+Copyright 2017 Dell Inc. or its subsidiaries. All Rights Reserved.
 
 Author(s):
 George Paulos
 
-OnRack/RackHD Functional Integration Test (FIT) library
-This is the main common function library for OnRack/RackHD FIT tests.
+RackHD Functional Integration Test (FIT) library
+This is the main common function library for RackHD FIT tests.
 '''
 
 # Standard imports
@@ -196,10 +196,10 @@ def apply_stack_config():
     stack = fitargs()['stack']
     if stack is not None:
         mkcfg().add_from_file('stack_config.json', stack)
-        if 'ora' in fitcfg():
-            fitargs()['ora'] = fitcfg()['ora']
+        if 'rackhd_host' in fitcfg():
+            fitargs()['rackhd_host'] = fitcfg()['rackhd_host']
         else:
-            fitargs()['ora'] = 'localhost'
+            fitargs()['rackhd_host'] = 'localhost'
         if 'bmc' in fitcfg():
             fitargs()['bmc'] = fitcfg()['bmc']
         if 'hyper' in fitcfg():
@@ -231,7 +231,7 @@ def add_globals():
         if API_PORT == "None":
             API_PORT = fitports()['https']
 
-    if fitargs()["ora"] == "localhost":
+    if fitargs()['rackhd_host'] == "localhost":
         if API_PROTOCOL == "None":
             API_PROTOCOL = 'http'
         if API_PORT == "None":
@@ -290,13 +290,11 @@ def mkargs(in_args=None):
     arg_parser.add_argument("-group", default="all",
                             help="test group to execute: 'smoke', 'regression', 'extended', default: 'all'")
     arg_parser.add_argument("-stack", default="vagrant",
-                            help="stack label (test bed), overrides -ora")
-    arg_parser.add_argument("-ora", default="localhost",
-                            help="OnRack/RackHD appliance IP address or hostname, default: localhost")
-    arg_parser.add_argument("-version", default="onrack-devel",
-                            help="OnRack package install version, example:onrack-release-0.3.0, default: onrack-devel")
+                            help="stack label (test bed)")
+    arg_parser.add_argument("-rackhd_host", default="localhost",
+                            help="RackHD appliance IP address or hostname, default: localhost")
     arg_parser.add_argument("-template", default="None",
-                            help="path or URL link to OVA template or OnRack OVA")
+                            help="path or URL link to OVA template or RackHD OVA")
     arg_parser.add_argument("-xunit", default="False", action="store_true",
                             help="generates xUnit XML report files")
     arg_parser.add_argument("-numvms", default=1, type=int,
@@ -410,7 +408,7 @@ def countdown(sleep_time, sleep_interval=1):
 def remote_shell(shell_cmd, expect_receive="", expect_send="", timeout=300,
                  address=None, user=None, password=None):
     '''
-    Run ssh based shell command on a remote machine at fitargs()["ora"]
+    Run ssh based shell command on a remote machine at fitargs()['rackhd_host']
 
     :param shell_cmd: string based command
     :param expect_receive:
@@ -422,11 +420,11 @@ def remote_shell(shell_cmd, expect_receive="", expect_send="", timeout=300,
     :return: dict = {'stdout': str:ouput, 'exitcode': return code}
     '''
     if not address:
-        address = fitargs()['ora']
+        address = fitargs()['rackhd_host']
     if not user:
-        user = fitcreds()['ora'][0]['username']
+        user = fitcreds()['rackhd_host'][0]['username']
     if not password:
-        password = fitcreds()['ora'][0]['password']
+        password = fitcreds()['rackhd_host'][0]['password']
 
     logfile_redirect = None
     if VERBOSITY >= 4:
@@ -438,7 +436,7 @@ def remote_shell(shell_cmd, expect_receive="", expect_send="", timeout=300,
         logfile_redirect = sys.stdout
 
     # if localhost just run the command local
-    if fitargs()['ora'] == 'localhost':
+    if fitargs()['rackhd_host'] == 'localhost':
         (command_output, exitstatus) = \
             pexpect.run("sudo bash -c \"" + shell_cmd + "\"",
                         withexitstatus=1,
@@ -473,9 +471,12 @@ def remote_shell(shell_cmd, expect_receive="", expect_send="", timeout=300,
 
 
 def scp_file_to_ora(src_file_name):
+    # legacy call
+    scp_file_to_host(src_file_name)
+
+def scp_file_to_host(src_file_name):
     '''
-    scp the given file over to the ORA and place it in onrack's
-    home directory.
+    scp the given file over to the RackHD host and place it in the home directory.
 
     :param src_file_name: name of file to copy over. May include path
     :type src_file_name: basestring
@@ -485,24 +486,24 @@ def scp_file_to_ora(src_file_name):
     logfile_redirect = file('/dev/null', 'w')
     just_fname = os.path.basename(src_file_name)
     # if localhost just copy to home dir
-    if fitargs()['ora'] == 'localhost':
+    if fitargs()['rackhd_host'] == 'localhost':
         remote_shell('cp ' + src_file_name + ' ~/' + src_file_name)
         return src_file_name
 
-    scp_target = fitcreds()['ora'][0]['username'] + '@{0}:'.format(fitargs()["ora"])
+    scp_target = fitcreds()['rackhd_host'][0]['username'] + '@{0}:'.format(fitargs()['rackhd_host'])
     cmd = 'scp -o StrictHostKeyChecking=no {0} {1}'.format(src_file_name, scp_target)
     if VERBOSITY >= 4:
-        print "scp_file_to_ora: '{0}'".format(cmd)
+        print "scp_file_to_host: '{0}'".format(cmd)
 
     if VERBOSITY >= 9:
         logfile_redirect = sys.stdout
 
     (command_output, ecode) = pexpect.run(
         cmd, withexitstatus=1,
-        events={'(?i)assword: ': fitcreds()['ora'][0]['password'] + '\n'},
+        events={'(?i)assword: ': fitcreds()['rackhd_host'][0]['password'] + '\n'},
         logfile=logfile_redirect)
     if VERBOSITY >= 4:
-        print "scp_file_to_ora: Exit Code = {0}".format(ecode)
+        print "scp_file_to_host: Exit Code = {0}".format(ecode)
 
     assert ecode == 0, \
         'failed "{0}" because {1}. Output={2}'.format(cmd, ecode, command_output)
@@ -516,17 +517,17 @@ def get_auth_token():
     api_login = {"username": fitcreds()["api"][0]["admin_user"], "password": fitcreds()["api"][0]["admin_pass"]}
     redfish_login = {"UserName": fitcreds()["api"][0]["admin_user"], "Password": fitcreds()["api"][0]["admin_pass"]}
     try:
-        restful("https://" + fitargs()['ora'] + ":" + str(API_PORT) +
+        restful("https://" + fitargs()['rackhd_host'] + ":" + str(API_PORT) +
                 "/login", rest_action="post", rest_payload=api_login, rest_timeout=2)
     except:
         AUTH_TOKEN = "Unavailable"
         return False
     else:
-        api_data = restful("https://" + fitargs()['ora'] + ":" + str(API_PORT) +
+        api_data = restful("https://" + fitargs()['rackhd_host'] + ":" + str(API_PORT) +
                            "/login", rest_action="post", rest_payload=api_login, rest_timeout=2)
         if api_data['status'] == 200:
             AUTH_TOKEN = str(api_data['json']['token'])
-            redfish_data = restful("https://" + fitargs()['ora'] + ":" + str(API_PORT) +
+            redfish_data = restful("https://" + fitargs()['rackhd_host'] + ":" + str(API_PORT) +
                                    "/redfish/v1/SessionService/Sessions",
                                    rest_action="post", rest_payload=redfish_login, rest_timeout=2)
             if 'x-auth-token' in redfish_data['headers']:
@@ -564,7 +565,7 @@ def rackhdapi(url_cmd, action='get', payload=[], timeout=None, headers={}):
     if API_PROTOCOL == "None":
         if API_PORT == "None":
             API_PORT = str(fitports()['http'])
-        if restful("http://" + fitargs()['ora'] + ":" + str(API_PORT) + "/", rest_timeout=2)['status'] == 0:
+        if restful("http://" + fitargs()['rackhd_host'] + ":" + str(API_PORT) + "/", rest_timeout=2)['status'] == 0:
             API_PROTOCOL = 'https'
             API_PORT = str(fitports()['https'])
         else:
@@ -575,7 +576,7 @@ def rackhdapi(url_cmd, action='get', payload=[], timeout=None, headers={}):
     if AUTH_TOKEN == "None":
         get_auth_token()
 
-    return restful(API_PROTOCOL + "://" + fitargs()['ora'] + ":" + str(API_PORT) + url_cmd,
+    return restful(API_PROTOCOL + "://" + fitargs()['rackhd_host'] + ":" + str(API_PORT) + url_cmd,
                    rest_action=action, rest_payload=payload, rest_timeout=timeout, rest_headers=headers)
 
 
@@ -777,7 +778,7 @@ def power_control_all_nodes(state):
 
 
 def mongo_reset():
-    # clears the Mongo database on ORA to default, returns 0 if successful
+    # clears the Mongo database on host to default, returns 0 if successful
     remote_shell('service onrack-conductor stop')
     remote_shell('/opt/onrack/bin/monorail stop')
     remote_shell("mongo pxe --eval 'db.dropDatabase\\\(\\\)'")
