2241975fde28bb7f850f03613835ab7fc05cdc34
paulog <george.paulos@emc.com>
2017-02-17 15:31:23 +0000

    Addressed Jims comments about deploy/os_ova_install.py

--- test/deploy/os_ova_install.py
+++ test/deploy/os_ova_install.py
@@ -13,11 +13,10 @@ usage:
     python run_tests.py -stack <stack ID> -test deploy/os_ova_install.py
 '''
 
+import fit_path  # NOQA: unused import
 import os
 import sys
 import subprocess
-# set path to common libraries
-sys.path.append(subprocess.check_output("git rev-parse --show-toplevel", shell=True).rstrip("\n") + "/test/common")
 import fit_common
 import pdu_lib
 
