7500d59ecdade1459bbcca6c6b87ddbefa3b18ff
paulog <george.paulos@emc.com>
2017-03-09 19:36:56 +0000

    FIx Hound violations

--- test/common/test_api_utils.py
+++ test/common/test_api_utils.py
@@ -22,13 +22,13 @@ import unicodedata
 
 
 def rackhd_get_node_list():
-    '''
+    """
     this function returns the list of node ids reported by
     the Monrail API /api/2.0/nodes
     :return:
         computer node list in that stack when success.
         otherwise on failure
-    '''
+    """
     monurl = "/api/2.0/nodes"
     mondata = fit_common.rackhdapi(url_cmd=monurl)
     nodes = mondata['json']
@@ -69,11 +69,11 @@ def get_rackhd_nodetype(nodeid):
 
 
 def get_node_list_by_type(node_type):
-    '''
+    """
     Get node list according to type (like PDU, compute)
     :param type:
     :return: node id list for given type
-    '''
+    """
     node_info = fit_common.rackhdapi(url_cmd="/api/2.0/nodes?type=" + node_type)
     node_list = []
     if node_info["status"] in [200, 201, 202, 204]:
@@ -83,11 +83,11 @@ def get_node_list_by_type(node_type):
 
 
 def delete_nodes_by_type(node_type):
-    '''
+    """
     Delete all nodes for a given node_type
     :param node_type:
     :return:
-    '''
+    """
     node_list = []
     node_list = get_node_list_by_type(node_type)
     if node_list == []:
@@ -107,14 +107,14 @@ def delete_nodes_by_type(node_type):
 
 
 def get_obm_port_ip(node_id):
-    '''
+    """
     Return the IP address of the management port of a compute node
 
     :param node_id: the id of the node.
     :return: String based ip address on success.            1 on URL error,
             2 on parsing error,
             otherwise on other errors
-    '''
+    """
 
     rest_res = {'json': "", 'text': "", 'status': "", 'headers': ""}
 
@@ -163,14 +163,14 @@ def get_obm_port_ip(node_id):
 
 
 def get_compute_bmc_ip(node_id):
-    '''
+    """
     Return the BMC IP address by given the ID of computing node.
     :param node_id: The node ID used for identify the computing node.
     :return: String of BMC ip address on success,
             1 on network error,
             2 on parsing error,
             otherwise on other errors
-    '''
+    """
 
     monorail_url = "/api/2.0/nodes/" + node_id + "/catalogs/bmc"
     rest_res = {'json': "", 'text': "", 'status': "", 'headers': ""}
@@ -189,14 +189,14 @@ def get_compute_bmc_ip(node_id):
 
 
 def get_compute_rmm_ip(node_id):
-    '''
+    """
     Return the RMM IP address by given the ID of computing node.
     :param node_id: The node ID used for identify the computing node.
     :return: String of RMM ip address on success,
             1 on network error,
             2 on parsing error,
             otherwise on other errors
-    '''
+    """
     monorail_url = "/api/2.0/nodes/" + node_id + "/catalogs/rmm"
     rest_res = {'json': "", 'text': "", 'status': "", 'headers': ""}
 
@@ -213,7 +213,7 @@ def get_compute_rmm_ip(node_id):
 
 
 def get_compute_node_username(node_id):
-    '''
+    """
     Get the username credential from node id
     :param node_id: The id given by OnRack
     :return:
@@ -222,7 +222,7 @@ def get_compute_node_username(node_id):
         2 on parsing error
         3 on unable to obtain password from username
         Otherwise
-    '''
+    """
     monorail_url = "/api/2.0/nodes/" + node_id
     rest_res = {'json': "", 'text': "", 'status': "", 'headers': ""}
     rest_res = fit_common.rackhdapi(url_cmd=monorail_url)
@@ -257,12 +257,12 @@ def get_compute_node_username(node_id):
 
 
 def get_relations_for_node(node_id):
-    '''
+    """
     Get encl id for a given node id
     Or get node id list for a give enclosure id
     :param node_id: node id
     :return: enclosure id list for a given node or node id list for a given enclosure
-    '''
+    """
 
     mon_url = "/api/2.0/nodes/" + node_id
     mon_data = fit_common.rackhdapi(url_cmd=mon_url)
@@ -280,11 +280,11 @@ def get_relations_for_node(node_id):
 
 
 def guess_the_password(str_username):
-    '''
+    """
     Guess the password according to the username
     :param str_username: string based username
     :return: string based password on success, 1 on unable to guess
-    '''
+    """
     dict_cheat_sheet = {}
     for entry in fit_common.fitcreds()['bmc']:
         dict_cheat_sheet[entry['username']] = entry['password']
@@ -296,13 +296,13 @@ def guess_the_password(str_username):
 
 
 def run_ipmi_command(node_ip, str_command, dict_credential):
-    '''
+    """
     Run ipmi command and check its return code
     :param node_ip: ip address of the server to run the ipmicommand against
     :param str_command:  the ipmitools string command
     :param dict_credential: the credential to run ipmitool command
     :return: result of remote_shell
-    '''
+    """
     remote_ssh_res = {'stdout': "", 'exitcode': 0}
     str_ipmi_cmd = "ipmitool -I lanplus -H " +\
                    node_ip + " -U " +\
@@ -319,13 +319,13 @@ def run_ipmi_command(node_ip, str_command, dict_credential):
 
 
 def run_ipmi_command_to_node(node_id, str_ipmi_cmd):
-    '''
+    """
     Run a ipmi command to the node_id
     :param node_id: the node in that OnRack
     :param str_ipmi_cmd: the option that is attached to ipmitool
     :return: running result on success.
              Different return codes otherwise.
-    '''
+    """
     str_ip = get_compute_bmc_ip(node_id)
     if (str_ip == 2 or str_ip == 1) or str_ip == '0.0.0.0':
         if fit_common.VERBOSITY >= 2:
@@ -345,9 +345,9 @@ def run_ipmi_command_to_node(node_id, str_ipmi_cmd):
 
 
 def get_poller_data_by_id(poller_id):
-    '''
+    """
     To get the poller data by poller id
-    '''
+    """
     poller_data = []
     monurl = "/api/2.0/pollers/" + str(poller_id) + "/data"
     mondata = fit_common.rackhdapi(url_cmd=monurl)
@@ -360,9 +360,9 @@ def get_poller_data_by_id(poller_id):
 
 
 def get_supported_pollers(node_id):
-    '''
+    """
     To get a poller dictionary, the keys are poller names, and the values are poller_id, poller_interval, etc
-    '''
+    """
     poller_dict = {}
 
     monurl = "/api/2.0/nodes/" + str(node_id) + "/pollers"
@@ -387,10 +387,10 @@ def get_supported_pollers(node_id):
 
 
 def get_ora_poller_id_list():
-    '''
+    """
     Get all poller ids for an onrack system
     :return: poller id list
-    '''
+    """
     mon_url = "/api/2.0/pollers"
     mon_data = fit_common.rackhdapi(url_cmd=mon_url)
     if mon_data['status'] not in [200, 201, 202, 204]:
@@ -406,11 +406,11 @@ def get_ora_poller_id_list():
 
 
 def get_catalogue_from_source(node_id, source):
-    '''
+    """
     To get the catalogue data by the source name
 
     Check the basic fields where there should be values, and return the data for the source
-    '''
+    """
     monurl = "/api/2.0/nodes/" + str(node_id) + "/catalogs/" + str(source)
     mondata = fit_common.rackhdapi(url_cmd=monurl)
 
@@ -426,10 +426,10 @@ def get_catalogue_from_source(node_id, source):
 
 
 def get_catalogue_sources(node_id):
-    '''
+    """
     To get the list of sources names from catalogue
     returns empty list on error
-    '''
+    """
     monurl = "/api/2.0/nodes/" + str(node_id) + "/catalogs"
     mondata = fit_common.rackhdapi(url_cmd=monurl)
 
@@ -449,12 +449,12 @@ def get_catalogue_sources(node_id):
 
 
 def get_node_source_id_list(node_id):
-    '''
+    """
     Get all catalog source ids for a given node
     :param node_id:
     :return: source name and source id as a dictionary
              empty dict if not found
-    '''
+    """
     ora_source_list = dict()
     mon_url = "/api/2.0/nodes/" + node_id + "/catalogs"
     mon_data = fit_common.rackhdapi(url_cmd=mon_url)
@@ -465,11 +465,11 @@ def get_node_source_id_list(node_id):
 
 
 def get_ora_source_id_list():
-    '''
+    """
     Get all catalog source ids for a onrack system
     :return: source id list
              empty list if not found
-    '''
+    """
     ora_source_list = []
     mon_url = "/api/2.0/catalogs"
     mon_data = fit_common.rackhdapi(url_cmd=mon_url)
@@ -480,11 +480,11 @@ def get_ora_source_id_list():
 
 
 def get_catalog_by_source_id(source_id):
-    '''
+    """
     Get catalog data by source id
     :param source_id: catalog source id
     :return: catalog data with specified source id
-    '''
+    """
     mon_url = "/api/2.0/catalogs/" + str(source_id)
     mon_data = fit_common.rackhdapi(url_cmd=mon_url)
     if mon_data['status'] in [200, 201, 202, 204]:
