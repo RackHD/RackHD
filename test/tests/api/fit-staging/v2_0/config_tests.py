bdd2ef06161a12e76f9c67062ceb32223a2a45e1
Erika Hohenstein <erika.hohenstein@emc.com>
2017-05-17 03:57:01 +0000

    Converted CIT formatted apiv2 scripts to FIT format

--- test/tests/api/fit-staging/v2_0/config_tests.py
+++ test/tests/api/fit-staging/v2_0/config_tests.py
@@ -1,44 +1,47 @@
-from config.api2_0_config import *
-from modules.logger import Log
+"""
+Copyright (c) 2016-2017 Dell Inc. or its subsidiaries. All Rights Reserved.
+"""
+import fit_path  # NOQA: unused import
+import fit_common
+import flogging
+
+from config.api2_0_config import config
 from on_http_api2_0 import ApiApi as Api
-from proboscis.asserts import assert_equal
-from proboscis.asserts import assert_false
-from proboscis.asserts import assert_raises
-from proboscis.asserts import assert_not_equal
-from proboscis.asserts import assert_true
-from proboscis import SkipTest
-from proboscis import test
-from json import dumps, loads
+from json import loads
+from nosedep import depends
+from nose.plugins.attrib import attr
 
-LOG = Log(__name__)
+logs = flogging.get_loggers()
 
 
-@test(groups=['config_api2.tests'])
-class ConfigTests(object):
-    def __init__(self):
+# @test(groups=['config_api2.tests'])
+@attr(regression=False, smoke=True, config_api2_tests=True)
+class ConfigTests(fit_common.unittest.TestCase):
+    def setUp(self):
         self.__client = config.api_client
 
-    @test(groups=['config_api2.tests', 'api2_check-config'])
-    def check_server_config(self):
-        """Testing GET:api/2.0/config to get server configuration"""
+    # @test(groups=['config_api2.tests', 'api2_check-config'])
+    def test_check_server_config(self):
+        # """Testing GET:api/2.0/config to get server configuration"""
         Api().config_get()
         rsp = self.__client.last_response
-        assert_equal(200, rsp.status, message=rsp.reason)
+        self.assertEqual(200, rsp.status, msg=rsp.reason)
 
-    @test(groups=['api2_patch-config'], depends_on_groups=['api2_check-config'])
-    def patch_server_config(self):
-        """Testing PATCH:api/2.0/config to patch a specific configuration item"""
+    # @test(groups=['api2_patch-config'], depends_on_groups=['api2_check-config'])
+    @depends(after=test_check_server_config)
+    def test_patch_server_config(self):
+        # """Testing PATCH:api/2.0/config to patch a specific configuration item"""
         test_pwd = {"PWD": "/this/is/a/test/for/patch_config"}
-        LOG.info("Patch PWD with a test path")
-        Api().config_patch(config = test_pwd)
+        logs.info(" Patch PWD with a test path")
+        Api().config_patch(config=test_pwd)
         server_config = loads(self.__client.last_response.data)
-        assert_equal(server_config.get('PWD'),'/this/is/a/test/for/patch_config', 'Oops patch config failed')
-        LOG.info("Doing a check config with test PWD")
+        self.assertEqual(server_config.get('PWD'), '/this/is/a/test/for/patch_config',
+                         msg='Oops patch config failed')
+        logs.info(" Doing a check config with test PWD")
         Api().config_get()
         rsp = self.__client.last_response
-        assert_equal(200, rsp.status, message=rsp.reason)
-        LOG.info("Restoring PWD config after patch test")
-        Api().config_patch(config = {"PWD": "/var/renasar/on-http"})
+        self.assertEqual(200, rsp.status, msg=rsp.reason)
+        logs.info(" Restoring PWD config after patch test")
+        Api().config_patch(config={"PWD": "/var/renasar/on-http"})
         rsp = self.__client.last_response
-        assert_equal(200, rsp.status, message=rsp.reason)
-
+        self.assertEqual(200, rsp.status, msg=rsp.reason)
--- test/tests/api/fit-staging/v2_0/pollers_tests.py
+++ test/tests/api/fit-staging/v2_0/pollers_tests.py
@@ -1,89 +1,105 @@
-from config.api2_0_config import *
-from modules.logger import Log
+'''
+Copyright (c) 2016-2017 Dell Inc. or its subsidiaries. All Rights Reserved.
+'''
+import fit_path  # NOQA: unused import
+import fit_common
+import flogging
+
+from config.api2_0_config import config
 from on_http_api2_0 import ApiApi as Api
 from on_http_api2_0.rest import ApiException
-from proboscis.asserts import *
-from proboscis import SkipTest
-from proboscis import test
-from json import dumps, loads
+from json import loads
+from nosedep import depends
+from nose.plugins.attrib import attr
 
-LOG = Log(__name__)
+logs = flogging.get_loggers()
 
-@test(groups=['pollers_api2.tests'])
-class PollersTests(object):
-    def __init__(self):
-        self.__client = config.api_client
 
-    @test(groups=['pollers_api2.tests', 'api2_get_pollers_library'])
-    def get_pollers_lib(self):
-        """Test GET:api/2.0/pollers/library"""
+# @test(groups=['pollers_api2.tests'])
+@attr(regression=False, smoke=True, pollers_api2_tests=True)
+class PollersTests(fit_common.unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        cls.__client = config.api_client
+        cls.pollers = []
+        cls.poller_libs = []
+        cls.created_pollers = []
+
+    # @test(groups=['pollers_api2.tests', 'api2_get_pollers_library'])
+    def test_get_pollers_lib(self):
+        # """Test GET:api/2.0/pollers/library"""
         Api().pollers_lib_get()
         result = self.__client.last_response
         data = loads(self.__client.last_response.data)
 
-        assert_equal(200, result.status, message=result.reason)
+        self.assertEqual(200, result.status, msg=result.reason)
 
         names = [lib['name'] for lib in data]
-        assert_true('ipmi' in names and 'snmp' in names, names)
+        self.assertTrue('ipmi' in names and 'snmp' in names, names)
 
         for lib in data:
-            assert_true('config' in lib.keys())
+            self.assertTrue('config' in lib.keys())
 
-        self._poller_libs = data
+        self.__class__.poller_libs = data
 
-    @test(groups=['pollers_api2.tests', 'api2_get_pollers_library_by_id'],
-          depends_on_groups=['api2_get_pollers_library'])
-    def get_pollers_lib_by_id(self):
-        """Test GET:api/2.0/pollers/library/:identifier"""
-        for lib in self._poller_libs:
+    # @test(groups=['pollers_api2.tests', 'api2_get_pollers_library_by_id'],
+    #      depends_on_groups=['api2_get_pollers_library'])
+    @depends(after='test_get_pollers_lib')
+    def test_get_pollers_lib_by_id(self):
+        # """Test GET:api/2.0/pollers/library/:identifier"""
+        for lib in self.__class__.poller_libs:
             Api().pollers_lib_by_id_get(identifier=lib['name'])
             result = self.__client.last_response
             data = loads(self.__client.last_response.data)
 
-            assert_equal(200, result.status, message=result.reason)
-            assert_equal(lib, data)
+            self.assertEqual(200, result.status, msg=result.reason)
+            self.assertEqual(lib, data)
 
         try:
             Api().pollers_lib_by_id_get(identifier='not_a_real_poller')
         except ApiException as e:
-            assert_true(404, e.status)
+            self.assertEqual(404, e.status, msg='Expected 404 status, received {}'.format(e.status))
 
-    @test(groups=['pollers_api2.tests', 'api2_get_pollers'])
-    def get_pollers(self):
-        """Test GET:api/2.0/pollers"""
+    # @test(groups=['pollers_api2.tests', 'api2_get_pollers'])
+    # no dependencies
+    def test_get_pollers(self):
+        # """Test GET:api/2.0/pollers"""
         Api().pollers_get()
         result = self.__client.last_response
         data = loads(self.__client.last_response.data)
 
-        assert_equal(200, result.status, message=result.reason)
+        self.assertEqual(200, result.status, msg=result.reason)
         for poller in data:
             keys = poller.keys()
-            assert_true('config' in keys)
-            assert_true('type' in keys)
-            assert_true('pollInterval' in keys)
+            self.assertTrue('config' in keys)
+            self.assertTrue('type' in keys)
+            self.assertTrue('pollInterval' in keys)
 
-        self._pollers = data
+        self.__class__.pollers = data
 
-    @test(groups=['pollers_api2.tests', 'api2_get_pollers_by_id'],
-          depends_on_groups=['api2_get_pollers'])
-    def get_pollers_by_id(self):
-        """Test GET:api/2.0/pollers/:identifier"""
-        for poller in self._pollers:
+    # @test(groups=['pollers_api2.tests', 'api2_get_pollers_by_id'],
+    #      depends_on_groups=['api2_get_pollers'])
+    @depends(after='test_get_pollers')
+    def test_get_pollers_by_id(self):
+        # """Test GET:api/2.0/pollers/:identifier"""
+        for poller in self.__class__.pollers:
             Api().pollers_id_get(identifier=poller['id'])
             result = self.__client.last_response
             data = loads(self.__client.last_response.data)
 
-            assert_equal(200, result.status, message=result.reason)
+            self.assertEqual(200, result.status, msg=result.reason)
             for key in ['pollInterval', 'paused', 'type', 'config']:
-                assert_equal(data[key], poller[key])
+                self.assertEqual(data[key], poller[key])
         try:
             Api().pollers_id_get(identifier='does_not_exist')
         except ApiException as e:
-            assert_true(404, e.status)
+            self.assertEqual(404, e.status, msg='Expected 404 status, received {}'.format(e.status))
 
-    @test(groups=['pollers_api2.tests', 'api2_create_pollers'])
-    def create_pollers(self):
-        """Test POST:api/2.0/pollers"""
+    # @test(groups=['pollers_api2.tests', 'api2_create_pollers'])
+    # no depends
+    def test_create_pollers(self):
+        # """Test POST:api/2.0/pollers"""
         pollers = [
             {
                 "type": "ipmi",
@@ -108,80 +124,83 @@ class PollersTests(object):
             }
         ]
 
-        self._created_pollers = []
+        self.__class__.created_pollers = []
         for poller in pollers:
             Api().pollers_post(poller)
             result = self.__client.last_response
             data = loads(self.__client.last_response.data)
             poller['config'].pop('password', None)
 
-            assert_equal(201, result.status, message=result.reason)
+            self.assertEqual(201, result.status, msg=result.reason)
             for key in poller.keys():
-                assert_equal(poller[key], data[key])
+                self.assertEqual(poller[key], data[key])
 
-            self._created_pollers.append(data)
+            self.__class__.created_pollers.append(data)
 
-    @test(groups=['pollers_api2.tests', 'api2_patch_pollers'],
-          depends_on_groups=['api2_create_pollers'])
-    def pollers_patch(self):
-        """Test PATCH:api/2.0/pollers/:identifier"""
+    # @test(groups=['pollers_api2.tests', 'api2_patch_pollers'],
+    #      depends_on_groups=['api2_create_pollers'])
+    @depends(after='test_create_pollers')
+    def test_pollers_patch(self):
+        # """Test PATCH:api/2.0/pollers/:identifier"""
         patch_data = {
             "pollInterval": 5000
         }
-        for poller in self._created_pollers:
+        for poller in self.__class__.created_pollers:
             Api().pollers_patch(poller['id'], patch_data)
             result = self.__client.last_response
             data = loads(self.__client.last_response.data)
 
-            assert_equal(200, result.status, message=result.reason)
-            assert_equal(5000, data['pollInterval'])
+            self.assertEqual(200, result.status, msg=result.reason)
+            self.assertEqual(5000, data['pollInterval'])
             poller = data
 
         try:
             Api().pollers_patch('does_not_exist', {})
         except ApiException as e:
-            assert_true(404, e.status)
+            self.assertEqual(404, e.status, msg='Expected 404 status, received {}'.format(e.status))
 
-    @test(groups=['pollers_api2.tests', 'api2_data_get_pollers'],
-          depends_on_groups=['api2_get_pollers', 'api2_patch_pollers'])
-    def pollers_data_get(self):
-        """Test GET:/api/2.0/pollers/:identifier/data"""
-        all_pollers = self._pollers + self._created_pollers
+    # @test(groups=['pollers_api2.tests', 'api2_data_get_pollers'],
+    #      depends_on_groups=['api2_get_pollers', 'api2_patch_pollers'])
+    @depends(after=['test_get_pollers', 'test_pollers_patch'])
+    def test_pollers_data_get(self):
+        # """Test GET:/api/2.0/pollers/:identifier/data"""
+        all_pollers = self.__class__.pollers + self.__class__.created_pollers
         for poller in all_pollers:
             Api().pollers_data_get(poller['id'])
             result = self.__client.last_response
-            assert_true(result.status == 200 or result.status == 204, message=result.reason)
+            self.assertTrue(result.status == 200 or result.status == 204, msg=result.reason)
             if result.status == 200:
-                assert_true(len(result.data) > 0, 'Poller data length should be non-zero')
+                self.assertTrue(len(result.data) > 0, 'Poller data length should be non-zero')
             else:
-                assert_equal(len(result.data), 0, 'Poller data should have 0 length')
+                self.assertEqual(len(result.data), 0, 'Poller data should have 0 length')
 
-    @test(groups=['pollers_api2.tests', 'api2_current_data_get_pollers'],
-          depends_on_groups=['api2_get_pollers', 'api2_data_get_pollers'])
-    def pollers_current_data_get(self):
-        """Test GET:/api/2.0/pollers/:identifier/data/current"""
-        all_pollers = self._pollers + self._created_pollers
+    # @test(groups=['pollers_api2.tests', 'api2_current_data_get_pollers'],
+    #      depends_on_groups=['api2_get_pollers', 'api2_data_get_pollers'])
+    @depends(after='test_pollers_data_get')
+    def test_pollers_current_data_get(self):
+        # """Test GET:/api/2.0/pollers/:identifier/data/current"""
+        all_pollers = self.__class__.pollers + self.__class__.created_pollers
         for poller in all_pollers:
             Api().pollers_data_get(poller['id'])
             result = self.__client.last_response
-            assert_true(result.status == 200 or result.status == 204, message=result.reason)
+            self.assertTrue(result.status == 200 or result.status == 204, msg=result.reason)
             if result.status == 200:
-                assert_true(len(result.data) > 0, 'Poller data length should be non-zero')
+                self.assertTrue(len(result.data) > 0, 'Poller data length should be non-zero')
             else:
-                assert_equal(len(result.data), 0, 'Poller data should have 0 length')
+                self.assertEqual(len(result.data), 0, 'Poller data should have 0 length')
 
-    @test(groups=['pollers_api2.tests', 'api2_delete_pollers'],
-          depends_on_groups=['api2_current_data_get_pollers'])
-    def delete_pollers(self):
-        """Test DELETE:api/2.0/pollers/:identifier"""
-        for poller in self._created_pollers:
+    # @test(groups=['pollers_api2.tests', 'api2_delete_pollers'],
+    #      depends_on_groups=['api2_current_data_get_pollers'])
+    @depends(after='test_pollers_current_data_get')
+    def test_delete_pollers(self):
+        # """Test DELETE:api/2.0/pollers/:identifier"""
+        for poller in self.__class__.created_pollers:
             Api().pollers_delete(identifier=poller["id"])
             result = self.__client.last_response
 
-            assert_equal(204, result.status, message=result.reason)
+            self.assertEqual(204, result.status, msg=result.reason)
 
         try:
             Api().pollers_delete(identifier='does_not_exist')
         except ApiException as e:
-            assert_true(404, e.status)
-
+            self.assertEqual(404, e.status, msg='Expected 404 status, received {}'.format(e.status))
