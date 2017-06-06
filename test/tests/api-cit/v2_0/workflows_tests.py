bf35111eb1e0f0bfecc2e7777395bdae96db2a6a
larry-dean <lawrence.dean@emc.com>
2017-06-06 10:56:48 -0500

    RAC-5128 - modify workflows test to use new AMQP * Choose node with sku packs * Change data to rsp_data to prevent overriding data * Added logic to skip test if AMQP server not active

--- test/tests/api-cit/v2_0/workflows_tests.py
+++ test/tests/api-cit/v2_0/workflows_tests.py
@@ -6,14 +6,12 @@ import fit_common
 import flogging
 
 from config.api2_0_config import config
-from config.amqp import QUEUE_GRAPH_FINISH
 from on_http_api2_0 import ApiApi as Api
 from on_http_api2_0.rest import ApiException
-from modules.amqp import AMQPWorker
-from modules.worker import WorkerThread, WorkerTasks
 from json import loads, dumps
 from nose.plugins.attrib import attr
 from nosedep import depends
+from sm_plugin import smp_get_stream_monitor
 
 logs = flogging.get_loggers()
 
@@ -27,7 +25,15 @@ class WorkflowsTests(fit_common.unittest.TestCase):
     @classmethod
     def setUpClass(cls):
         cls.__client = config.api_client
-        # cls.__task_worker = None
+        # Get the stream-monitor plugin for AMQP
+        cls._amqp_sp = smp_get_stream_monitor('amqp')
+
+        if cls._amqp_sp and cls._amqp_sp.has_amqp_server:
+            # Create the "all events" tracker
+            cls._on_events_tracker = cls._amqp_sp.create_tracker('wf-on-events-all', 'on.events', '#')
+        else:
+            cls._on_events_tracker = None
+
         cls.__graph_name = None
         cls.__tasks = []
         cls.__graph_status = []
@@ -52,10 +58,24 @@ class WorkflowsTests(fit_common.unittest.TestCase):
             ]
         }
 
+    def setUp(self):
+        if not self._on_events_tracker:
+            raise fit_common.unittest.SkipTest('Skipping AMQP test because no AMQP server defined')
+
+        # attach a processor to the on-events-tracker amqp tracker. Then we can
+        # attach indiviual match-clauses to this in each test-case.
+        self.__qproc = self._amqp_sp.get_tracker_queue_processor(self._on_events_tracker, start_at='now')
+
+    def __get_result(self):
+        return self.__client.last_response
+
+    def __get_data(self):
+        return loads(self.__get_result().data)
+
     def test_delete_all_active_workflows(self):
         # """Testing node PUT:/nodes/identifier/workflows/action"""
         Api().nodes_get_all()
-        nodes = loads(self.__client.last_response.data)
+        nodes = self.__get_data()
         for node in nodes:
             if node.get('type') == 'compute':
                 id = node.get('id')
@@ -69,16 +89,16 @@ class WorkflowsTests(fit_common.unittest.TestCase):
     def test_workflows_get(self):
         # """ Testing GET:/workflows"""
         Api().workflows_get()
-        self.assertEqual(200, self.__client.last_response.status)
-        self.assertNotEqual(0, len(loads(self.__client.last_response.data)),
+        self.assertEqual(200, self.__get_result().status)
+        self.assertNotEqual(0, len(self.__get_data()),
                             msg='Active workflows list was empty!')
 
     @depends(after='test_workflows_get')
     def test_workflows_post(self):
         # """Testing POST:/workflows"""
         Api().workflows_post(body={"name": 'Graph.noop-example'})
-        self.assertEqual(201, self.__client.last_response.status)
-        rawj = loads(self.__client.last_response.data)
+        self.assertEqual(201, self.__get_result().status)
+        rawj = loads(self.__get_result().data)
         instance_id = rawj.get('instanceId')
         self.assertIsNotNone(instance_id)
         self.assertEqual('Graph.noop-example', str(rawj['definition'].get('injectableName')))
@@ -89,18 +109,18 @@ class WorkflowsTests(fit_common.unittest.TestCase):
 
         # Getting the identifier of the first workflow in order to validate the get-id function
         Api().workflows_get()
-        rawj = loads(self.__client.last_response.data)
+        rawj = self.__get_data()
         instance_id = rawj[0].get('instanceId')
         self.assertIsNotNone(instance_id)
         Api().workflows_get_by_instance_id(instance_id)
-        self.assertEqual(200, self.__client.last_response.status)
+        self.assertEqual(200, self.__get_result().status)
 
     @depends(after='test_workflows_get')
     def test_negative_workflows_id_get(self):
         # """ Negative Testing GET:/workflows/identifier"""
         try:
             Api().workflows_get_by_instance_id("WrongIdentifier")
-            self.assertEqual(404, self.__client.last_response.status, msg='status should be 404. No exception raised')
+            self.assertEqual(404, self.__get_result(), msg='status should be 404. No exception raised')
         except ApiException as e:
             self.assertEqual(404, e.status, msg='Expected 404 status, received {}'.format(e.status))
         except (TypeError, ValueError) as e:
@@ -109,10 +129,10 @@ class WorkflowsTests(fit_common.unittest.TestCase):
     def test_workflows_graphs_get(self):
         # """Testing GET:/workflows/graphs"""
         Api().workflows_get_graphs()
-        self.assertEqual(200, self.__client.last_response.status)
-        resp = loads(self.__client.last_response.data)
+        self.assertEqual(200, self.__get_result().status)
+        resp = self.__get_data()
         logs.debug_6(" Workflow graphs: %s", dumps(resp, indent=4))
-        self.assertNotEqual(0, len(loads(self.__client.last_response.data)),
+        self.assertNotEqual(0, len(self.__get_data()),
                             msg='Workflows list was empty!')
 
     def test_workflows_graphs_put(self):
@@ -120,7 +140,7 @@ class WorkflowsTests(fit_common.unittest.TestCase):
 
         # Make sure there is no workflowTask with the same name
         Api().workflows_get_graphs_by_name('*')
-        rawj = loads(self.__client.last_response.data)
+        rawj = self.__get_data()
 
         for i, var in enumerate(rawj):
             if self.workflowDict['injectableName'] == str(rawj[i].get('injectableName')):
@@ -132,12 +152,11 @@ class WorkflowsTests(fit_common.unittest.TestCase):
         # Add a workflow task
         logs.info("Adding workflow task: %s ", str(self.workflowDict))
         Api().workflows_put_graphs(body=self.workflowDict)
-        resp = self.__client.last_response
-        self.assertEqual(201, resp.status)
+        self.assertEqual(201, self.__get_result().status)
 
         # Validate the content
         Api().workflows_get_graphs()
-        rawj = loads(self.__client.last_response.data)
+        rawj = self.__get_data()
         foundInsertedWorkflow = False
         for i, var in enumerate(rawj):
             if self.workflowDict['injectableName'] == str(rawj[i].get('injectableName')):
@@ -154,8 +173,8 @@ class WorkflowsTests(fit_common.unittest.TestCase):
     def test_workflows_library_id_get(self):
         # """ Testing GET:/workflows/graphs/injectableName"""
         Api().workflows_get_graphs_by_name(self.workflowDict.get('injectableName'))
-        self.assertEqual(200, self.__client.last_response.status)
-        rawj = loads(self.__client.last_response.data)
+        self.assertEqual(200, self.__get_result().status)
+        rawj = self.__get_data()
         self.assertEqual(self.workflowDict.get('friendlyName'), str(rawj[0].get('friendlyName')))
 
     @depends(after='test_workflows_library_id_get')
@@ -163,26 +182,32 @@ class WorkflowsTests(fit_common.unittest.TestCase):
         # """Testing PUT:/workflows/graphs"""
         # Test updating a graph
         Api().workflows_get_graphs_by_name(self.workflowDict.get('injectableName'))
-        rawj = loads(self.__client.last_response.data)
+        rawj = self.__get_data()
         self.assertEqual(self.workflowDict.get('friendlyName'), str(rawj[0].get('friendlyName')))
         Api().workflows_put_graphs(body=self.workflowDict2)
-        self.assertEqual(201, self.__client.last_response.status)
+        self.assertEqual(201, self.__get_result().status)
         Api().workflows_get_graphs_by_name(self.workflowDict.get('injectableName'))
-        rawj = loads(self.__client.last_response.data)
+        rawj = self.__get_data()
         self.assertEqual(self.workflowDict2.get('friendlyName'), str(rawj[0].get('friendlyName')))
 
     @depends(after='test_workflows_graphs_name_put')
     def test_workflows_graphs_delete(self):
         # """Testing DELETE:/workflows/graphs/injectableName"""
         Api().workflows_get_graphs_by_name(self.workflowDict.get('injectableName'))
-        rawj = loads(self.__client.last_response.data)
+        rawj = self.__get_data()
         self.assertEqual(self.workflowDict2.get('friendlyName'), str(rawj[0].get('friendlyName')))
         Api().workflows_delete_graphs_by_name(self.workflowDict.get('injectableName'))
-        self.assertEqual(204, self.__client.last_response.status)
+        self.assertEqual(204, self.__get_result().status)
         Api().workflows_get_graphs_by_name(self.workflowDict.get('injectableName'))
-        self.assertEqual(0, len(loads(self.__client.last_response.data)))
+        self.assertEqual(0, len(self.__get_data()))
 
-    def post_workflows(self, graph_name, timeout_sec=10, nodes=[], data=None, tasks=None, callback=None, run_now=True):
+    @depends(after=['test_workflows_graphs_name_put', 'test_delete_all_active_workflows'])
+    def test_node_workflows_post(self):
+        # """Testing POST:/nodes/id/workflows"""
+        self.post_workflows("Graph.noop-example")
+
+    def post_workflows(self, graph_name, timeout_sec=10, nodes=[], data=None,
+                       tasks=None, callback=None, run_now=True):
         self.__class__.__graph_name = graph_name
         self.__class__.__graph_status = []
 
@@ -192,90 +217,53 @@ class WorkflowsTests(fit_common.unittest.TestCase):
 
         if len(nodes) == 0:
             Api().nodes_get_all()
-            nodes = loads(self.__client.last_response.data)
-
-        if callback is None:
-            logs.info("handle graph finish")
-            callback = self.handle_graph_finish
+            nodes = self.__get_data()
 
         for n in nodes:
-            if n.get('type') == 'compute':
+            if n.get('type') == 'compute' and n.get('sku'):
                 logs.debug("node is compute")
-                id = n.get('id')
-                self.assertIsNotNone(id)
-                logs.debug(' Starting amqp listener for node %s', id)
-                worker = AMQPWorker(queue=QUEUE_GRAPH_FINISH, callbacks=[callback])
-                thread = WorkerThread(worker, id)
-                self.__class__.__tasks.append(thread)
-                tasks.append(thread)
-                try:
-                    Api().nodes_workflow_action_by_id(id, {'command': 'cancel'})
-                except ApiException as e:
-                    self.assertEqual(404, e.status, msg='Expected 404 status, received {}'.format(e.status))
-                except (TypeError, ValueError) as e:
-                    assert(e.message)
-                Api().nodes_post_workflow_by_id(id, name=self.__class__.__graph_name, body=data)
-                logs.info("Posted workflow %s on node %s", self.__class__.__graph_name, id)
+                node_id = n.get('id')
+                self.assertIsNotNone(node_id)
+                logs.debug(' Starting amqp listener for node %s', node_id)
 
-        if run_now:
-            logs.info("running workflow tasks....")
-            self.run_workflow_tasks(self.__class__.__tasks, timeout_sec)
+                # make sure to cannel any possible active workflow
+                self.__cancel_active_workflow(node_id)
 
-    def handle_graph_finish(self, body, message):
-        routeId = message.delivery_info.get('routing_key').split('graph.finished.')[1]
-        self.assertIsNotNone(routeId)
-        Api().workflows_get()
-        workflows = loads(self.__client.last_response.data)
-        message.ack()
-        for w in workflows:
-            injectableName = w['injectableName']
-            if injectableName == self.__class__.__graph_name:
-                graphId = w['context'].get('graphId')
-                if graphId == routeId:
-                    if 'target' in w['context']:
-                        nodeid = w['context']['target'] or 'none'
-                    else:
-                        nodeid = 'none'
-                    status = body['status']
-                    if status == 'succeeded' or status == 'failed':
-                        logs.info('%s - target: %s, status: %s, route: %s',
-                                  injectableName, nodeid, status, routeId)
-                        self.__class__.__graph_status.append(status)
+                # post new workflow
+                Api().nodes_post_workflow_by_id(node_id, name=self.__class__.__graph_name, body=data)
+                logs.info("Posted workflow %s on node %s", self.__class__.__graph_name, node_id)
+                rsp_data = self.__get_data()
+                workflow_id = rsp_data['instanceId']
+                logs.info("*************** Posted workflow id %s on node %s", workflow_id, node_id)
 
-                        for task in self.__class__.__tasks:
-                            if task.id == nodeid:
-                                task.worker.stop()
-                                task.running = False
+                self.__qproc.match_on_routekey('post-graph-finish',
+                                               routing_key='graph.finished.{}'.format(workflow_id),
+                                               validation_block=self.__build_workflow_node_post_block(workflow_id))
 
-                        msg = {
-                            'graph_name': injectableName,
-                            'target': nodeid,
-                            'status': status,
-                            'route_id': routeId
-                        }
+                self.__qproc.match_on_routekey('post-graph-finish-infomation',
+                                               routing_key='graph.finished.information.{}.{}'.format(workflow_id, node_id))
 
-                        if status == 'failed':
-                            msg['active_task'] = w['tasks']
-                            logs.error(dumps(msg, indent=4))
-                        else:
-                            logs.info(dumps(msg, indent=4))
-                        break
+        results = self._amqp_sp.finish(timeout=360)
+        results[0].assert_errors(self)
 
-    def run_workflow_tasks(self, tasks, timeout_sec):
-        def thread_func(worker, id):
-            worker.start()
-        tasks = self.__class__.__tasks if tasks is None else tasks
-        worker_tasks = WorkerTasks(tasks=self.__class__.__tasks, func=thread_func)
-        worker_tasks.run()
-        worker_tasks.wait_for_completion(timeout_sec=timeout_sec)
-        for task in tasks:
-            if task.timeout:
-                logs.error('Timeout for %s, node %s', self.__class__.__graph_name, task.id)
-                self.__class__.__graph_status.append('failed')
-        if 'failed' in self.__class__.__graph_status:
-            self.fail('Failure running {}'.format(self.__class__.__graph_name))
+    def __cancel_active_workflow(self, node_id):
+        # Cancel any possible running workflows
+        try:
+            Api().nodes_workflow_action_by_id(node_id, {'command': 'cancel'})
+        except ApiException as e:
+            self.assertEqual(404, e.status, msg='Expected 404 status, received {}'.format(e.status))
+        except (TypeError, ValueError) as e:
+            assert(e.message)
 
-    @depends(after=['test_delete_all_active_workflows', 'test_workflows_graphs_delete'])
-    def test_node_workflows_post(self):
-        # """Testing POST:/nodes/id/workflows"""
-        self.post_workflows("Graph.noop-example")
+    def __build_workflow_node_post_block(self, workflow_id):
+        expected_payload = {
+            "status": 'succeeded'
+        }
+
+        expected_rk = "graph.finished.{}".format(workflow_id)
+
+        ex = {
+            'body': expected_payload,
+            'routing_key': expected_rk
+        }
+        return ex
